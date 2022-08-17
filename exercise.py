9d499d9efff611b6e64801ea9d0a728a87fa5e69
writehereyourname <writehereyouremail>
2022-08-17 10:44:27 +0200

    save

--- exercise.py
+++ exercise.py
@@ -2,4 +2,70 @@
 # upload to github for portfolio
 # thank me later when your git graph is as green as the python money u gonna earn
 
+#Task1ex6
+
+'''         
+
+three_mul = 'fizz'
+five_mul = 'buzz'
+num1 = 3
+num2 = 5 
+max_num = 100
+   
+for i in range(1,max_num):
+    # % or modulo division gives you the remainder 
+    
+    if i%num1 == 0:
+        print(i, three_mul)
+    elif i%num2 == 0:
+        print(i, five_mul)
+    if i%num1 == 0 and i%num2 == 0:
+        print(i, three_mul + five_mul)
+
+'''
+
+#Task2ex6
+
+'''
+
+n = 5
+number = 1
+sum = 0
+while number < n + 1:
+    sum = sum + number
+    number = number + 1
+print("Sum =", sum)
+
+'''
+
+#Task3ex6
+
+'''
+
+n = 5
+sum = 0
+for num in range(n + 1):
+    sum += num
+print("Sum =", sum)
+
+'''
+
+#Task4ex6
+
+
+
+for x in range(3):
+    print(x)
+
+for j in range(5):
+    print("This is loop number", j)    
+
+xy = 10
+while xy <= 0:
+    print(xy - 1)
+    xy -= 10
+    
+    
+
+#Task5ex6
 
